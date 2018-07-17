441816e4c19ea22a1930f4827cec7bdb677b50c5
Bofei Zhu <zhu.bofei@gmail.com>
2018-07-17 02:18:33 -0400

    Update `ListUpdatingDelegate`

--- Source/ListUpdatingDelegate.swift
+++ Source/ListUpdatingDelegate.swift
@@ -6,16 +6,14 @@
 //  Copyright © 2018 Bofei Zhu. All rights reserved.
 //
 
-/**
- A completion closure to execute when updates are finished.
- - Parameter finished: Specifies whether or not the update finished.
- */
+/// A completion closure to execute when updates are finished.
+///
+/// - Parameter finished: Specifies whether or not the update finished.
 public typealias ListUpdatingCompletion = (_ finished: Bool) -> Void
 
-/**
- A closure to be called when the adapter applies changes to the collection view.
- - Parameter toObjects: The new objects in the collection.
- */
+///  A closure to be called when the adapter applies changes to the collection view.
+///
+/// - Parameter toObjects: The new objects in the collection.
 public typealias ListObjectTransitionClosure = (_ toObjects: [AnyListDiffable]) -> Void
 
 /// A closure that contains all of the updates.
@@ -30,32 +28,30 @@ public typealias ListToObjectClosure = () -> [AnyListDiffable]?
 /// A closure that returns a collection view to perform updates on.
 public typealias ListCollectionViewClosure = () -> UICollectionView?
 
-/**
- Implement this protocol in order to handle both section and row based update events.
- Implementation should forward or coalesce these events to a backing store or collection.
- */
+/// Implement this protocol in order to handle both section and row based update events.
+/// Implementation should forward or coalesce these events to a backing store or collection.
 public protocol ListUpdatingDelegate: AnyObject {
-    /**
-     Tells the delegate to perform a section transition from an old array of objects to a new one.
-     - Parameters:
-        - collectionViewClosure: A closure returning the collecion view to perform updates on.
-        - fromObjects: The previous objects in the collection view.
-            Objects must conform to `ListDiffable`.
-        - toObjectsClosure: A closure returning the new objects in the collection view.
-            Objects must conform to `ListDiffable`.
-        - animated: A flag indicating if the transition should be animated.
-        - objectTransitionClosure: A closure that must be called when the adapter applies
-            changes to the collection view.
-        - completion: A completion closure to execute when the update is finished.
-     
-     - Note: Implementations determine how to transition between objects.
-     You can perform a diff on the objects, reload each section, or simply call `reloadData()`
-     on the collection view. In the end, the collection view must be setup with a section
-     for each object in the `toObjects` array.
-     
-     The `objectTransitionClosure` closure should be called prior to making any
-     `UICollectionView` updates, passing in the `toObjects` that the updater is applying.
-     */
+    
+    /// Tells the delegate to perform a section transition from an old array of objects to
+    /// a new one.
+    ///
+    /// - Parameters:
+    ///   - collectionViewClosure: A closure returning the collecion view to perform updates on.
+    ///   - fromObjects: The previous objects in the collection view.
+    ///         Objects must conform to `ListDiffable`.
+    ///   - toObjectsClosure: A closure returning the new objects in the collection view.
+    ///         Objects must conform to `ListDiffable`.
+    ///   - animated: A flag indicating if the transition should be animated.
+    ///   - objectTransitionClosure: A closure that must be called when the adapter applies
+    ///         changes to the collection view.
+    ///   - completion: A completion closure to execute when the update is finished.
+    /// - Note: Implementations determine how to transition between objects.
+    /// You can perform a diff on the objects, reload each section, or simply call `reloadData()`
+    /// on the collection view. In the end, the collection view must be setup with a section
+    /// for each object in the `toObjects` array.
+    ///
+    /// The `objectTransitionClosure` closure should be called prior to making any
+    /// `UICollectionView` updates, passing in the `toObjects` that the updater is applying.
     func performUpdateWith(
         collectionViewClosure: @escaping ListCollectionViewClosure,
         fromObjects: [AnyListDiffable]?,
@@ -63,95 +59,85 @@ public protocol ListUpdatingDelegate: AnyObject {
         animated: Bool,
         objectTransitionClosure: @escaping ListObjectTransitionClosure,
         completion: ListUpdatingCompletion?)
-    
-    /**
-     Perform an item update closure in the collection view.
-     - Parameters:
-        - collectionViewClosure: A closure returning the collecion view to perform updates on.
-        - animated: A flag indicating if the transition should be animated.
-        - itemUpdates: A closure containing all of the updates.
-        - completion: A completion closure to execute when the update is finished.
-     */
+
+    /// Perform an item update closure in the collection view.
+    ///
+    /// - Parameters:
+    ///   - collectionViewClosure: A closure returning the collecion view to perform updates on.
+    ///   - animated: A flag indicating if the transition should be animated.
+    ///   - itemUpdates: A closure containing all of the updates.
+    ///   - completion: A completion closure to execute when the update is finished.
     func performUpdateWith(
         collectionViewClosure: @escaping ListCollectionViewClosure,
         animated: Bool,
         itemUpdates: @escaping ListItemUpdateClosure,
         completion: ListUpdatingCompletion?)
-    
-    /**
-     Tells the delegate to perform item inserts at the given index paths.
-     - Parameters:
-        - collectionView: The collection view on which to perform the transition.
-        - indexPaths: The index paths to insert items into.
-     */
+
+    /// Tells the delegate to perform item inserts at the given index paths.
+    ///
+    /// - Parameters:
+    ///   - collectionView: The collection view on which to perform the transition.
+    ///   - indexPaths: The index paths to insert items into.
     func collectionView(_ collectionView: UICollectionView, insertItemsAt indexPaths: [IndexPath])
-    
-    /**
-     Tells the delegate to perform item deletes at the given index paths.
-     - Parameters:
-        - collectionView: The collection view on which to perform the transition.
-        - indexPaths: The index paths to delete items from.
-     */
+
+    /// Tells the delegate to perform item deletes at the given index paths.
+    ///
+    /// - Parameters:
+    ///   - collectionView: The collection view on which to perform the transition.
+    ///   - indexPaths: The index paths to delete items from.
     func collectionView(_ collectionView: UICollectionView, deleteItemsAt indexPaths: [IndexPath])
-    
-    /**
-     Tells the delegate to move an item from and to given index paths.
-     - Parameters:
-        - collectionView: The collection view on which to perform the transition.
-        - indexPath: The source index path of the item to move.
-        - newIndexPath: The destination index path of the item to move.
-     */
+
+    /// Tells the delegate to move an item from and to given index paths.
+    ///
+    /// - Parameters:
+    ///   - collectionView: The collection view on which to perform the transition.
+    ///   - indexPath: The source index path of the item to move.
+    ///   - newIndexPath: The destination index path of the item to move.
     func collectionView(
         _ collectionView: UICollectionView,
         moveItemAt indexPath: IndexPath,
         to newIndexPath: IndexPath)
-    
-    /**
-     Tells the delegate to reload an item from and to given index paths.
-     - Parameters:
-        - collectionView: The collection view on which to perform the transition.
-        - indexPath: The source index path of the item to reload.
-        - newIndexPath: The destination index path of the item to reload.
-     
-     - Note: Since `UICollectionView` is unable to handle calling
-     `UICollectionView.reloadItems(at:)` safely while also executing insert and delete operations
-     in the same batch updates, the updater must know about the origin and destination of the reload
-     to perform a safe transition.
-     */
+
+    /// Tells the delegate to reload an item from and to given index paths.
+    ///
+    /// - Parameters:
+    ///   - collectionView: The collection view on which to perform the transition.
+    ///   - indexPath: The source index path of the item to reload.
+    ///   - newIndexPath: The destination index path of the item to reload.
+    /// - Note: Since `UICollectionView` is unable to handle calling
+    ///     `UICollectionView.reloadItems(at:)` safely while also executing insert and delete operations
+    ///     in the same batch updates, the updater must know about the origin and destination of the reload
+    ///     to perform a safe transition.
     func collectionView(
         _ collectionView: UICollectionView,
         reloadItemAt indexPath: IndexPath,
         to newIndexPath: IndexPath)
     
-    /**
-     Tells the delegate to move a section from and to given indexes.
-     - Parameters:
-        - collectionView: The collection view on which to perform the transition.
-        - section: The source index of the section to move.
-        - newSection: The destination index of the section to move.
-     */
+    /// Tells the delegate to move a section from and to given indexes.
+    ///
+    /// - Parameters:
+    ///   - collectionView: The collection view on which to perform the transition.
+    ///   - section: The source index of the section to move.
+    ///   - newSection: The destination index of the section to move.
     func collectionView(
         _ collectionView: UICollectionView,
         moveSection section: Int,
         toSection newSection: Int)
-    
-    /**
-     Completely reload each section in the collection view.
-     - Parameters:
-        - collectionView: The collection view to reload.
-        - sections: The sections to reload.
-     */
-    
+
+    /// Completely reload each section in the collection view.
+    ///
+    /// - Parameters:
+    ///   - collectionView: The collection view to reload.
+    ///   - sections: The sections to reload.
     func collectionView(_ collectionView: UICollectionView, reloadSections sections: IndexSet)
-    
-    /**
-     Completely reload data in the collection.
-     - Parameters:
-        - collectionViewClosure: A closure returning the collecion view to reload.
-        - reloadUpdateClosure: A closure that must be called
-            when the adapter reloads the collection view.
-        - completion: A completion closure to execute when the reload is finished.
-     */
+
+    /// Completely reload data in the collection.
+    ///
+    /// - Parameters:
+    ///   - collectionViewClosure: A closure returning the collecion view to reload.
+    ///   - reloadUpdateClosure: A closure that must be called when the adapter reloads
+    ///         the collection view.
+    ///   - completion: A completion closure to execute when the reload is finished.
     func reloadDataWith(
         collectionViewClosure: @escaping ListCollectionViewClosure,
         reloadUpdateClosure: @escaping ListReloadUpdateClosure,
