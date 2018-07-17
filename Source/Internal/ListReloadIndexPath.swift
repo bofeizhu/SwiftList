4ddf87d1ba5ea71d2b74561fedeb11323e01a16a
Bofei Zhu <zhu.bofei@gmail.com>
2018-07-17 09:22:56 -0400

    nit

--- Source/Internal/ListReloadIndexPath.swift
+++ Source/Internal/ListReloadIndexPath.swift
@@ -6,26 +6,20 @@
 //  Copyright © 2018 Bofei Zhu. All rights reserved.
 //
 
-/**
- An object with index path information for reloading an item during a batch update.
- */
+/// An object with index path information for reloading an item during a batch update.
 struct ListReloadIndexPath {
-    /**
-     The index path of the item before batch updates are applied.
-     */
+    
+    /// The index path of the item before batch updates are applied.
     let from: IndexPath
     
-    /**
-     The index path of the item after batch updates are applied.
-     */
+    /// The index path of the item after batch updates are applied.
     let to: IndexPath
     
-    /**
-     Creates a new reload object.
-     - Parameters:
-        - from: The index path of the item before batch updates.
-        - to: The index path of the item after batch updates.
-     */
+    /// Creates a new reload object.
+    ///
+    /// - Parameters:
+    ///    - from: The index path of the item before batch updates.
+    ///    - to: The index path of the item after batch updates.
     init(from: IndexPath, to: IndexPath) {
         self.from = from
         self.to = to
