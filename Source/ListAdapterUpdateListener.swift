39154ee8af9e3f7735808ccb162a427f8e336a46
Bofei Zhu <zhu.bofei@gmail.com>
2018-07-17 23:58:42 -0400

    Create `ListAdapterUpdateListener`

---
+++ Source/ListAdapterUpdateListener.swift
@@ -0,0 +1,10 @@
+//
+//  ListAdapterUpdateListener.swift
+//  ListKit
+//
+//  Created by Bofei Zhu on 7/17/18.
+//  Copyright © 2018 Bofei Zhu. All rights reserved.
+//
+
+protocol ListAdapterUpdateListener: AnyObject {
+}
