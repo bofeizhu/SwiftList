94175bb405a7770a2b1116d7a43fc99b48622f28
Bofei Zhu <zhu.bofei@gmail.com>
2018-07-20 00:58:03 -0400

    Finished `ListScrollDelegate`

--- Source/ListScrollDelegate.swift
+++ Source/ListScrollDelegate.swift
@@ -8,4 +8,39 @@
 
 public protocol ListScrollDelegate: AnyObject {
     
+    /// Tells the delegate that the section controller was scrolled on screen.
+    ///
+    /// - Parameters:
+    ///   - listAdapter: The list adapter whose collection view was scrolled.
+    ///   - sectionController: The visible section controller that was scrolled.
+    func listAdapter(_ listAdapter: ListAdapter, didScroll sectionController: ListSectionController)
+    
+    /// Tells the delegate that the section controller will be dragged on screen.
+    ///
+    /// - Parameters:
+    ///   - listAdapter: The list adapter whose collection view will drag.
+    ///   - sectionController: The visible section controller that will drag.
+    func listAdapter(
+        _ listAdapter: ListAdapter,
+        willBeginDragging sectionController: ListSectionController)
+    
+    /// Tells the delegate that the section controller did end dragging on screen.
+    ///
+    /// - Parameters:
+    ///   - listAdapter: The list adapter whose collection view ended dragging.
+    ///   - sectionController: The visible section controller that ended dragging.
+    ///   - decelerate: `true` if section controller will decelerate, `false` otherwise.
+    func listAdapter(
+        _ listAdapter: ListAdapter,
+        didEndDragging sectionController: ListSectionController,
+        willDecelerate decelerate: Bool)
+    
+    /// Tells the delegate that the section controller did end decelerating on screen.
+    ///
+    /// - Parameters:
+    ///   - listAdapter: The list adapter whose collection view ended decelerating.
+    ///   - sectionController: The visible section controller that ended decelerating.
+    func listAdapter(
+        _ listAdapter: ListAdapter,
+        didEndDecelerating sectionController: ListSectionController)
 }
