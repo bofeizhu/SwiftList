5e678c46c675b150038a45b973af52349f83500e
Bofei Zhu <zhu.bofei@gmail.com>
2018-07-24 21:20:51 -0400

    Finished `ListSectionMapTests`

--- Tests/ListSectionMapTests.swift
+++ Tests/ListSectionMapTests.swift
@@ -11,7 +11,7 @@ import XCTest
 
 class ListSectionMapTests: XCTestCase {
     func testWhenUpdatingItemsThatArraysAreEqual() {
-        let objects = [1, 2, 3].typeErased()
+        let objects = [0, 1, 2].typeErased()
         let sectionControllers = [
             ListSectionController(),
             ListSectionController(),
@@ -20,5 +20,94 @@ class ListSectionMapTests: XCTestCase {
         var map = ListSectionMap()
         map.update(objects: objects, withSectionControllers: sectionControllers)
         XCTAssertEqual(objects, map.objects)
+    }
+    
+    func testWhenUpdatingItemsThatSectionControllersAreMappedForSection() {
+        let objects = [0, 1, 2].typeErased()
+        let sectionControllers = [
+            ListSectionController(),
+            ListSectionController(),
+            ListSectionController(),
+        ]
+        var map = ListSectionMap()
+        map.update(objects: objects, withSectionControllers: sectionControllers)
+        XCTAssertEqual(map.sectionController(forSection: 1)!, sectionControllers[1])
+    }
+    
+    func testWhenUpdatingItemsThatSectionControllersAreMappedForItem() {
+        let objects = [0, 1, 2].typeErased()
+        let sectionControllers = [
+            ListSectionController(),
+            ListSectionController(),
+            ListSectionController(),
+        ]
+        var map = ListSectionMap()
+        map.update(objects: objects, withSectionControllers: sectionControllers)
+        XCTAssertEqual(map.sectionController(for: AnyListDiffable(1))!, sectionControllers[1])
+    }
+    
+    func testQhenUpdatingItemsThatSectionsAreMappedForSectionController() {
+        let objects = [0, 1, 2].typeErased()
+        let sectionControllers = [
+            ListSectionController(),
+            ListSectionController(),
+            ListSectionController(),
+        ]
+        var map = ListSectionMap()
+        map.update(objects: objects, withSectionControllers: sectionControllers)
+        XCTAssertEqual(map.section(for: sectionControllers[1])!, 1)
+    }
+    
+    func testWhenUpdatingItemsWithUnknownItemThatSectionControllerIsNil() {
+        let objects = [0, 1, 2].typeErased()
+        let sectionControllers = [
+            ListSectionController(),
+            ListSectionController(),
+            ListSectionController(),
+        ]
+        var map = ListSectionMap()
+        map.update(objects: objects, withSectionControllers: sectionControllers)
+        XCTAssertNil(map.sectionController(for: AnyListDiffable(4)))
+    }
+    
+    func testWhenUpdatingItemsWithSectionControllerThatSectionIsNil() {
+        let objects = [0, 1, 2].typeErased()
+        let sectionControllers = [
+            ListSectionController(),
+            ListSectionController(),
+            ListSectionController(),
+        ]
+        var map = ListSectionMap()
+        map.update(objects: objects, withSectionControllers: sectionControllers)
+        XCTAssertNil(map.section(for: ListSectionController()))
+    }
+    
+    func testWhenAccessingOOBSectionThatNilIsReturned() {
+        let objects = [0, 1, 2].typeErased()
+        let sectionControllers = [
+            ListSectionController(),
+            ListSectionController(),
+            ListSectionController(),
+        ]
+        var map = ListSectionMap()
+        map.update(objects: objects, withSectionControllers: sectionControllers)
+        XCTAssertNil(map.object(forSection: 4))
+    }
+    
+    func testWhenUpdatingItemsThatSectionControllerIndexesAreUpdated() {
+        let objects = [0, 1, 2].typeErased()
+        let one = ListSectionController()
+        XCTAssertNil(one.section)
+        
+        let sectionControllers = [
+            ListSectionController(),
+            one,
+            ListSectionController(),
+        ]
+        var map = ListSectionMap()
+        map.update(objects: objects, withSectionControllers: sectionControllers)
+        
+        XCTAssertEqual(one.section!, 1)
+        XCTAssertFalse(one.isFirstSection)
     }
 }
