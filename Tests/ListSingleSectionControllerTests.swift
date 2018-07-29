d93f2af0220d205a9e571cb5e07b382b9f98bcef
Bofei Zhu <zhu.bofei@gmail.com>
2018-07-29 11:52:58 -0400

    update tests

--- Tests/ListSingleSectionControllerTests.swift
+++ Tests/ListSingleSectionControllerTests.swift
@@ -26,4 +26,18 @@ class ListSingleSectionControllerTests: ListTestCase {
         XCTAssertEqual(collectionView.numberOfItems(inSection: 1), 1)
         XCTAssertEqual(collectionView.numberOfItems(inSection: 2), 1)
     }
+    
+    func testWhenDisplayingCollectionViewThatCellsAreConfigured() {
+        setupWith([
+            ListTestObject(key: 1, value: "foo"),
+            ListTestObject(key: 2, value: "bar"),
+            ListTestObject(key: 3, value: "baz"),
+        ].typeErased())
+        let cell0 = collectionView.cellForItem(at: IndexPath(item: 0, section: 0)) as! ListTestCell
+        let cell1 = collectionView.cellForItem(at: IndexPath(item: 0, section: 1)) as! ListTestCell
+        let cell2 = collectionView.cellForItem(at: IndexPath(item: 0, section: 2)) as! ListTestCell
+        XCTAssertEqual(cell0.label.text, "foo")
+        XCTAssertEqual(cell1.label.text, "bar")
+        XCTAssertEqual(cell2.label.text, "baz")
+    }
 }
